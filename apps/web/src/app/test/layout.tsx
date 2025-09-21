9ac278194464cd7b127986c45d8a00211e1b4494
lucasfeklein <lucas.fklein@hotmail.com>
2025-09-21 12:03:36 -0300

    test

--- apps/web/src/app/test/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { ConvexClientProvider } from "../ConvexClientProvider";
-
-export default function TestLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <ConvexClientProvider>{children}</ConvexClientProvider>;
-}
